ec1bcb60e6ec6d256658bfaac3908151cc8c805e
Christopher Ho <christopher.ho@skipthedishes.ca>
2019-03-27 20:34:21 -0500

    Fix devtools error for redux

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const middleware = [thunk];
 
 const store = createStore(rootReducers, 
                           compose(applyMiddleware(...middleware), 
-                          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+                          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f));
 
 export default class App extends Component {
   render() {
